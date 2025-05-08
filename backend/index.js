8618bfb7b43186bcfe0e8a849ef545d709111032
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-09 01:26:38 +0600

    MongoDB connection successful

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
 
