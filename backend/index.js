ae1dbc17a5e90cc8febac7622cff4aac54501bdc
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-09 01:36:19 +0600

    working on auth route and controller

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js'; 
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // allows us to parse incoming requests:req.body
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
   connectDB();
