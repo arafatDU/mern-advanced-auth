a32e79de053674dfcfe9d753f69fea9f29e1d6bb
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-13 18:38:27 +0600

    Reset password api endpoint  created

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,6 +1,7 @@
 import {
 	VERIFICATION_EMAIL_TEMPLATE,
 	PASSWORD_RESET_REQUEST_TEMPLATE,
+	PASSWORD_RESET_SUCCESS_TEMPLATE,
 } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
@@ -65,5 +66,26 @@ export const sendPasswordResetEmail = async (email, resetURL) => {
 		console.error(`Error sending password reset email`, error);
 
 		throw new Error(`Error sending password reset email: ${error}`);
+	}
+};
+
+
+export const sendResetSuccessEmail = async (email) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Password Reset Successful",
+			html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+			category: "Password Reset",
+		});
+
+		console.log("Password reset email sent successfully", response);
+	} catch (error) {
+		console.error(`Error sending password reset success email`, error);
+
+		throw new Error(`Error sending password reset success email: ${error}`);
 	}
 };
